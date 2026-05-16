b51a6a5e3148e7a8f66a77597209602726c5dd1f
Валерия Краснова <k-valeria-s@yandex.ru>
2026-05-16 15:48:03 +0500

    edit migration for sqlite

--- database/migrations/2026_04_30_000003_create_master_classes_table.php
+++ database/migrations/2026_04_30_000003_create_master_classes_table.php
@@ -17,7 +17,8 @@ return new class extends Migration
             $table->string('title');
             $table->text('description');
             $table->date('date');
-            $table->enum('time_slot', ['9-11', '11-13', '13-15', '15-17']);
+            $table->string('time_slot')->default('9-11');
+            #$table->enum('time_slot', ['9-11', '11-13', '13-15', '15-17']);
             $table->integer('max_participants');
             $table->integer('price');
             $table->timestamps();

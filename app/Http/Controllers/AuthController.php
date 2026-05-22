be3fcb5b5ece8ec2610ed13827ef19a181a6d9d8
Валерия Краснова <k-valeria-s@yandex.ru>
2026-05-22 21:33:24 +0500

    end checking dev: correct code

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -18,7 +18,7 @@ class AuthController extends Controller
 {
     public function showLoginForm(): View
     {
-        return 123;
+        return view('auth.login');
     }
 
     public function showRegisterForm(): View

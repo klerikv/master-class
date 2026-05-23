ff4b6279c1b3e12a22ecd0eabf32f438bb35b9f8
Валерия Краснова <k-valeria-s@yandex.ru>
2026-05-23 23:59:27 +0500

    check dev: incorrect type (larastan error)

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -18,7 +18,7 @@ class AuthController extends Controller
 {
     public function showLoginForm(): View
     {
-        return view('auth.login')   ;
+        return 123;
     }
 
     public function showRegisterForm(): View

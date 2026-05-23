8dd683f6ed6ed6d35a870bb57ab9c2eca8bcf639
Валерия Краснова <k-valeria-s@yandex.ru>
2026-05-23 23:55:45 +0500

    check dev: add extra space (linting error)

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -18,7 +18,7 @@ class AuthController extends Controller
 {
     public function showLoginForm(): View
     {
-        return view('auth.login');
+        return view('auth.login')   ;
     }
 
     public function showRegisterForm(): View
